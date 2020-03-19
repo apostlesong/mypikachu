df74fef8c27a9538ac3b4884bb6cd35dcdf22030
song <apostlesong@gmail.com>
2020-03-19 16:15:55 +0800

    03191614

--- src/main/java/init/CardTableResetHibernate.java
+++ src/main/java/init/CardTableResetHibernate.java
@@ -3,33 +3,22 @@ package init;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Blob;
 import java.sql.Clob;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-import _00_init.util.DBService;
-import _00_init.util.GlobalService;
-import _00_init.util.HibernateUtils;
-import _01_register.model.MemberBean;
-import _03_listBooks.model.BookBean;
 import _03_listBooks.model.CompanyBean;
+import card.CardsBean;
+import member.model.MemberBean;
 
-// 本程式建立範例程式所需要的表格與部分表格的初始資料
-// 範例程式所需要的表格如下：
-// 
+// 本程式建立CARDS的表格
+ 
 public class CardTableResetHibernate {
-	public static final String UTF8_BOM = "\uFEFF"; // 定義 UTF-8的BOM字元
+//	public static final String UTF8_BOM = "\uFEFF"; // 定義 UTF-8的BOM字元
 
 	public static void main(String args[]) {
 		SessionFactory factory = HibernateUtils.getSessionFactory();
@@ -41,69 +30,95 @@ public class CardTableResetHibernate {
 		// 1. BookCompany表格
 		// 1-1 由"data/bookCompany.dat"逐筆讀入BookCompany表格內的初始資料，
 		// 然後依序新增到BookCompany表格中
-		try (
-			FileReader fr = new FileReader("data/bookCompany.dat"); 
-			BufferedReader br = new BufferedReader(fr);
-		) {
-			while ((line = br.readLine()) != null) {
-				if (line.startsWith(UTF8_BOM)) {
-					line = line.substring(1);
-				}
-				String[] token = line.split("\\|");
-				String name = token[0];
-				String address = token[1];
-				String url = token[2];
-				CompanyBean cb = new CompanyBean(null, name, address, url);
-				try {
-					tx = session.beginTransaction();
-					session.save(cb);
-					tx.commit();
-				} catch (Exception ex) {
-					if (tx != null)
-						tx.rollback();
-				}
-			}
-			System.out.println("BookCompany 資料新增成功");
-		} catch (IOException e) {
-			System.err.println("新建BookCompany表格時發生IO例外: " + e.getMessage());
-		}
+//		try (
+//			FileReader fr = new FileReader("data/cardcsv.csv"); 
+//			BufferedReader br = new BufferedReader(fr);
+//		) {
+//			while ((line = br.readLine()) != null) {
+//				if (line.startsWith(UTF8_BOM)) {
+//					line = line.substring(1);
+//				}
+//				String[] token = line.split("\\|");
+//				String name = token[0];
+//				String address = token[1];
+//				String url = token[2];
+//				CompanyBean cb = new CompanyBean(null, name, address, url);
+//				try {
+//					tx = session.beginTransaction();
+//					session.save(cb);
+//					tx.commit();
+//				} catch (Exception ex) {
+//					if (tx != null)
+//						tx.rollback();
+//				}
+//			}
+//			System.out.println("BookCompany 資料新增成功");
+//		} catch (IOException e) {
+//			System.err.println("新建BookCompany表格時發生IO例外: " + e.getMessage());
+//		}
 
 		// 2. Book表格
-		File file = new File("data/book.dat");
-		// 2-1 由"data/book.dat"逐筆讀入Book表格內的初始資料，然後依序新增
-		// 到Book表格中
+		File file = new File("data/cardcsv.csv");
+		
 		try (
 			FileInputStream fis = new FileInputStream(file);
-			InputStreamReader isr = new InputStreamReader(fis, "UTF8");
+			InputStreamReader isr = new InputStreamReader(fis, "BIG5");
 			BufferedReader br = new BufferedReader(isr);
-		) {
+		) { int brcnt ;
+		        brcnt = 0;
 			while ((line = br.readLine()) != null) {
-				System.out.println("line=" + line);
-				// 去除 UTF8_BOM: \uFEFF
-				if (line.startsWith(UTF8_BOM)) {
-					line = line.substring(1);
-				}
-				String[] token = line.split("\\|");
-				BookBean book = new BookBean();
-				book.setTitle(token[0]);
-				book.setAuthor(token[1]);
-				book.setListPrice(Double.parseDouble(token[2].trim()));
-				book.setDiscount(Double.parseDouble(token[3].trim()));
-				book.setCompanyId(Integer.parseInt(token[4].trim()));
-				// 讀取圖片檔
-				Blob blob = GlobalService.fileToBlob(token[5].trim());
-				book.setCoverImage(blob);
-				book.setFileName(GlobalService.extractFileName(token[5].trim()));
-				book.setBookNo(token[6].trim());
-				book.setStock(Integer.parseInt(token[7].trim()));
-				book.setCategory(token[8].trim());
+				 brcnt++;
+					if (count < 2) {
+						continue;
+					}
+					System.out.println("line=" + line);
+					
+					String[] token = line.split(",");
+				
+
+				CardsBean cb = new CardsBean();
+//				cb.setC_id();
+				cb.setC_name(token[1]);
+				cb.setBank(token[2]);
+				cb.setAnnlfee(token[3]);
+				cb.setC_img(blob);(Integer.parseInt(token[4].trim()));
+				cb.setFcb(Double.parseDouble(token[5].trim()));
+				cb.setDcb(Double.parseDouble(token[6].trim()));
+				cb.setFma(token[7]) 	;		
+				cb.setDma(token[8]) 	;		
+				cb.setFma_n(token[9]) 	;		
+				cb.setDma_n(token[10]) 	;		
+				cb.setRefuel_cb(Double.parseDouble(token[11].trim())) ;		
+				cb.setRefuel_dis(token[12]) 	;	
+				cb.setRefuel_cb_n(token[13]) ;	
+				cb.setRefuel_dis_n(token[14]) ;
+				cb.setWkdmoviedis(token[15]) 	;
+				cb.setMoviedis(token[16]) 		;
+				cb.setWkdmoviedis_n(token[17]) ;
+				cb.setMoviedis_n(token[18]) 		;
+				cb.setDitigalfb(token[19]) 		;
+				cb.setOnlinefb(token[20]) 		;
+				cb.setDitigalfb_n(token[21]) 	;
+				cb.setOnlinefb_n(token[22]) 		;
+				cb.setZeroins(token[23])		;
+				cb.setMinins(token[24])			;
+				cb.setZeroins_n(token[25])		;
+				cb.setMinins_n(token[26])		;
+				cb.setMileagefb(token[27])		;
+				cb.setCashfb(token[28]) 			;
+				cb.setMileagefb_n(token[29]) 	;
+				cb.setCashfb_n(token[30]) 		;
+				cb.setAnnual_income(Integer.parseInt(token[31].trim())) ;
+				cb.setC_type(token[32]) 		  ;
+				
 				try {
 					tx = session.beginTransaction();
-					CompanyBean companyBean = 
-							session.get(CompanyBean.class, Integer.parseInt(token[4].trim()));
-					book.setCompanyBean(companyBean);
+				//	設外鍵
+//					CompanyBean companyBean = 
+//							session.get(CompanyBean.class, Integer.parseInt(token[4].trim()));
+//					book.setCompanyBean(companyBean);
 					
-					session.save(book);
+					session.save(cb);
 					tx.commit();
 				} catch (Exception ex) {
 					if (tx != null)
